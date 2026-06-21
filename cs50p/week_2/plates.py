89d2810d289cb81aba31ae17d7ca1444f81c794b
michalgbb <michalgbb@op.pl>
2026-06-21 22:24:45 +0200

    Aktualizacja 21.06.2026

--- cs50p/week_2/plates.py
+++ cs50p/week_2/plates.py
@@ -7,40 +7,50 @@ def main():
 
 
 def is_valid(s):
-
-    if 2 <= len(s) <= 6 and s[0:2].isalpha() and s[2:6].isalnum() and s[2] != "0":
-# For 6 letters        
-        if  len(s) == 6 and (s[2].isdigit() or s[3].isdigit() or s[4].isdigit()) and s[5].isalpha():
-            return False
-        elif  len(s) == 6 and (s[2].isdigit() or s[3].isdigit()) and s[4].isalpha():
-            return False
-        elif  len(s) == 6 and s[2].isdigit() and s[3].isalpha():
-            return False
-# For 5 letters
-        elif len(s) == 5 and (s[2].isdigit() or s[3].isdigit()) and s[4].isalpha():
-            return False
-        
-        elif len(s) == 5 and s[2].isdigit() and s[3].isalpha() and s[4].isdigit():
-            return False
-# For 4 letters
-        elif len(s) == 4 and s[2].isdigit() and s[3].isalpha():
-            return False
-        else:
-            return True           
-    else:
-        return False
-
-    # plates = []
+    count = False
     
-    # for l in s:
-    #     plates.append(l)
+    if len(s) >= 2 and len(s) <= 6 and s[0:2].isalpha() and s.isalnum():   
         
-    # for plate in plates[0:2]:
-    #         if plate.isalpha():
-                
-    #         else:
-    #             return False
-                 
-            
+        for letter in s:
+            # Sprawdzamy zero i jezeli wystepuje zmieniamy przelącznik
+            if letter == "0" and count == False:
+                count = True
+                return False
+            # Pojawienie się liczby i włączenie przełącznika
+            elif letter.isdigit():
+                count = True
+            # Pojawienie się litery włączonym przełączniku (liczbie)
+            elif letter.isalpha() and count == True:
+                return False         
+        
+        return True       
+   
+    return False
+
+#     def is_valid(s):
+
+#     if 2 <= len(s) <= 6 and s[0:2].isalpha() and s[2:6].isalnum() and s[2] != "0":
+# # For 6 letters        
+#         if  len(s) == 6 and (s[2].isdigit() or s[3].isdigit() or s[4].isdigit()) and s[5].isalpha():
+#             return False
+#         elif  len(s) == 6 and (s[2].isdigit() or s[3].isdigit()) and s[4].isalpha():
+#             return False
+#         elif  len(s) == 6 and s[2].isdigit() and s[3].isalpha():
+#             return False
+# # For 5 letters
+#         elif len(s) == 5 and (s[2].isdigit() or s[3].isdigit()) and s[4].isalpha():
+#             return False
+        
+#         elif len(s) == 5 and s[2].isdigit() and s[3].isalpha() and s[4].isdigit():
+#             return False
+# # For 4 letters
+#         elif len(s) == 4 and s[2].isdigit() and s[3].isalpha():
+#             return False
+#         else:
+#             return True           
+#     else:
+#         return False
+
 
 main()
+
